d63f87400f0331c2846605231bf9b4cd5df74572
Hyso Coder <kalebavincent@gmail.com>
2025-02-18 19:28:04 +0100

    Update StickerId.py
    J'ai ajouter from info import *

--- plugins/modules/StickerId.py
+++ plugins/modules/StickerId.py
@@ -8,6 +8,7 @@ from decouple import config
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import User, Message, Sticker, Document
+from info import *
 
     
 bughunter0 = Client(
